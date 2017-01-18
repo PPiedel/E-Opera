46e15f20cbf1457f78714a05768486c9f28fafa0
PPiedel <pawelpiedel@yahoo.pl>
2017-01-18 11:25:13 +0100

    Back button implemented.

--- src/main/resources/js/podglad.js
+++ src/main/resources/js/podglad.js
@@ -3,29 +3,59 @@
  */
 
 document.onreadystatechange = function () {
-    console.log('podglad_sali_ready');
     if (document.readyState == "complete") {
         addListenersForPlaces();
+        addListenerForPreviusButton();
     }
 };
 
 function addListenersForPlaces() {
     $('.table-row td').click(function() {
         var value = this.getAttribute('value');
-        var color  = $(this).css("background-color");
-        console.log(color);
 
         if ($(this).css("background-color") == "rgba(0, 0, 0, 0)" ){
-            console.log('zmieniam kolor na czerwony');
             $(this).addClass('active');
             $(this).css('backgroundColor', 'rgb(255, 0, 0)');
         }
         else if ($(this).css("background-color") == "rgb(255, 0, 0)" ) {
-            console.log('zmieniam kolor na bialy');
             $(this).removeClass('active');
             $(this).css('backgroundColor', 'rgba(0, 0, 0, 0)');
         }
 
     })
 
+}
+
+function addListenerForPreviusButton() {
+    $('.buttons #previousButton').click(function() {
+        window.history.back();
+    })
+
+}
+
+function writeCookie(name,value,days) {
+    var date, expires;
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        expires = "; expires=" + date.toGMTString();
+    }else{
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var i, c, ca, nameEQ = name + "=";
+    ca = document.cookie.split(';');
+    for(i=0;i < ca.length;i++) {
+        c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
+        }
+    }
+    return '';
 }
