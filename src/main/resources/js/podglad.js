dad267b18ecea0ecfd1ac9611a5318c9eeff2cd2
PPiedel <pawelpiedel@yahoo.pl>
2017-01-18 11:11:06 +0100

    Colors changing of places after marking works now in podglad.html

--- src/main/resources/js/podglad.js
+++ src/main/resources/js/podglad.js
@@ -12,6 +12,20 @@ document.onreadystatechange = function () {
 function addListenersForPlaces() {
     $('.table-row td').click(function() {
         var value = this.getAttribute('value');
+        var color  = $(this).css("background-color");
+        console.log(color);
+
+        if ($(this).css("background-color") == "rgba(0, 0, 0, 0)" ){
+            console.log('zmieniam kolor na czerwony');
+            $(this).addClass('active');
+            $(this).css('backgroundColor', 'rgb(255, 0, 0)');
+        }
+        else if ($(this).css("background-color") == "rgb(255, 0, 0)" ) {
+            console.log('zmieniam kolor na bialy');
+            $(this).removeClass('active');
+            $(this).css('backgroundColor', 'rgba(0, 0, 0, 0)');
+        }
+
     })
 
 }
